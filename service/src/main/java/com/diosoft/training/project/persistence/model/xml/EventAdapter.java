5e85a4d5eff7cb7045a785d0df3c3c0688206d19
oleksandr_khomenko <oleksandr88khomenko@gmail.com>
2014-11-19 17:13:43 +0200

    added event and person adapters

---
+++ service/src/main/java/com/diosoft/training/project/persistence/model/xml/EventAdapter.java
@@ -0,0 +1,117 @@
+package com.diosoft.training.project.persistence.model.xml;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by oleksandr_khomenko on 20.11.2014.
+ */
+@XmlRootElement
+@XmlType(name = "event")
+public class EventAdapter implements Serializable {
+
+    private Long id;
+    private String title;
+    private String description;
+    private List<PersonAdapter> attenders;
+    private Date dateFrom;
+    private Date dateTo;
+
+
+    public Long get_id() {
+        return id;
+    }
+
+    public void set_id(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDateFrom(Date dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+
+    public void setDateTo(Date dateTo) {
+        this.dateTo = dateTo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDateFrom() {
+        return dateFrom;
+    }
+
+    public Date getDateTo() {
+        return dateTo;
+    }
+
+    @XmlElementWrapper(name = "attenders")
+    @XmlElement(name = "attender")
+    public List<PersonAdapter> getAttenders() {
+        return attenders;
+    }
+
+    public void setAttenders(List<PersonAdapter> attenders) {
+        this.attenders = attenders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EventAdapter that = (EventAdapter) o;
+
+        if (!attenders.equals(that.attenders)) return false;
+        if (!dateFrom.equals(that.dateFrom)) return false;
+        if (!dateTo.equals(that.dateTo)) return false;
+        if (!description.equals(that.description)) return false;
+        if (!id.equals(that.id)) return false;
+        if (!title.equals(that.title)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + title.hashCode();
+        result = 31 * result + description.hashCode();
+        result = 31 * result + attenders.hashCode();
+        result = 31 * result + dateFrom.hashCode();
+        result = 31 * result + dateTo.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "EventAdapter{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", attenders=" + attenders +
+                ", dateFrom=" + dateFrom +
+                ", dateTo=" + dateTo +
+                '}';
+    }
+}
+
+
---
+++ service/src/main/java/com/diosoft/training/project/persistence/model/xml/PersonAdapter.java
@@ -0,0 +1,83 @@
+package com.diosoft.training.project.persistence.model.xml;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+/**
+ * Created by oleksandr_khomenko on 20.11.2014.
+ */
+@XmlRootElement
+public class PersonAdapter implements Serializable {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonAdapter that = (PersonAdapter) o;
+
+        if (!email.equals(that.email)) return false;
+        if (!id.equals(that.id)) return false;
+        if (!password.equals(that.password)) return false;
+        if (!username.equals(that.username)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + username.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + email.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonAdapter{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
